e0997648c0ebc2fe5dbb518e4d483dd1a043de1e
Mauricio G <mau.gar.gnz@gmail.com>
2019-09-07 16:07:04 -0500

    subiendo web

---
+++ PoseNet_webcam/sketch.js
@@ -0,0 +1,54 @@
+let video;
+let poseNet;
+let poses = [];
+
+function setup() {
+  createCanvas(640, 480);
+  video = createCapture(VIDEO);
+  video.size(width, height);
+
+  // Create a new poseNet method with a single detection
+  poseNet = ml5.poseNet(video, modelReady);
+  // This sets up an event that fills the global variable "poses"
+  // with an array every time new poses are detected
+  poseNet.on('pose', function(results) {
+    poses = results;
+  });
+  // Hide the video element, and just show the canvas
+  video.hide();
+}
+
+function modelReady() {
+  select('#status').html('Model Loaded');
+}
+
+function draw() {
+  image(video, 0, 0, width, height);
+
+  // We can call both functions to draw all keypoints and the skeletons
+  drawKeypoints();
+}
+
+let banana = 5;
+let sandia = 5;
+
+// A function to draw ellipses over the detected keypoints
+function drawKeypoints()  {
+  // Loop through all the poses detected
+  for (let i = 0; i < poses.length; i++) {
+    // For each pose detected, loop through all the keypoints
+    let pose = poses[i].pose;
+    for (let j = 0; j < pose.keypoints.length; j++) {
+      // A keypoint is an object describing a body part (like rightArm or leftShoulder)
+      let keypoint = pose.keypoints[j];
+      // Only draw an ellipse is the pose probability is bigger than 0.2
+      if (keypoint.score > 0.99) {
+        fill(255, 0, 0);
+        noStroke();
+        rect(keypoint.position.x, keypoint.position.y, 5, 13);
+        //ellipse(keypoint.position.x+banana, keypoint.position.y+sandia, 100, 100);
+        //console.log(ellipse(keypoint.position.x, keypoint.position.y, 1, 1))
+      }
+    }
+  }
+}
